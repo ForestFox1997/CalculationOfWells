9acedbac3e9c28ff5a7a58448d165a57aa2a0008
kostylev-as <kostylev-as@cityair.ru>
2026-04-04 03:23:18 +0700

    Реализована база MVVM взаимодействия

---
+++ CalculationOfWells/ViewModels/TestViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace CalculationOfWells.ViewModels
+{
+    public partial class TestViewModel : ObservableObject
+    {
+        public TestViewModel()
+        {
+            Text = "Начальное значение";
+            MyCommand = new RelayCommand(() => Text = "Привязка работает!");
+        }
+
+        // генерирует свойство string Text и уведомления
+        [ObservableProperty]
+        private string _text;
+
+        // команда, которая поменяет Text
+        public IRelayCommand MyCommand { get; }
+    }
+}
---
+++ CalculationOfWells/Views/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using CalculationOfWells.ViewModels;
+
+namespace CalculationOfWells.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new TestViewModel();
+        }
+    }
+}
